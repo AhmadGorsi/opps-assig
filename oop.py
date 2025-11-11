1e0d6328dc165c706faac923d8f62731c4a41155
AhmadGorsi <iahmadbilal2@gmail.com>
2025-11-11 18:39:17 +0500

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,14 +1,16 @@
 class Student:
-    def __init__(self,name,mark1,mark2,):
+    def __init__(self,name,mark1,mark2,mark3):
         self.name=name
         self.mark1=mark1
         self.mark2=mark2
+        self.mark3=mark3
     def average(self):
-        return self.mark1+self.mark2/2
+        return self.mark1+self.mark2+mark3/2
     print(average)
-std = Student('Ahmad',66,88)
+std = Student('Ahmad',66,88,56)
 
 
       
 
         
+
